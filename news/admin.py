624263ad0b8df6c3a2f1b5140ddeed387186f0e7
Barış Gündüzer <barisgunduzer@users.noreply.github.com>
2020-07-26 21:30:31 +0300

    post(news) admin resim galerisi oluşturuldu

--- news/admin.py
+++ news/admin.py
@@ -1,17 +1,26 @@
 # Register your models here.
 
 from django.contrib import admin
+from news.models import Post, Category, Picture
 
-from news.models import Post, Category
+class PostImageInline(admin.TabularInline):
+    model = Picture
+    extra = 3
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title','status']
+    list_display = ['title', 'status']
     list_filter = ['status']
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title','category','status']
-    list_filter = ['status','category']
+    list_display = ['title', 'category', 'status']
+    list_filter = ['status', 'category']
+    inlines = [PostImageInline]
+
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ['title', 'post', 'image']
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Post,PostAdmin)
+admin.site.register(Picture,PictureAdmin)
+
 
--- news/models.py
+++ news/models.py
@@ -27,7 +27,6 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    image = models.ImageField(blank=True, upload_to='images')
     t_header = models.CharField(max_length=255)
     t_content = models.TextField()
     status = models.CharField(max_length=10, choices=STATUS)
@@ -35,6 +34,14 @@ class Post(models.Model):
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Picture(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images')
 
     def __str__(self):
         return self.title
